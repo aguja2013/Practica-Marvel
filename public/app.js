b3e662dd3b3455082a7e8297842609f15cfe3c78
Agustin Palmieri <aguja13@hotmail.com>
2015-06-17 12:31:03 -0300

    Clase Ajax con jQuery

---
+++ public/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var $ = window.jQuery;
+
+$.get('http://gateway.marvel.com:80/v1/public/series?title=avengers&apikey=a548aee0bde874ea460773884934a865').done(function (results) {
+  debugger;
+});
