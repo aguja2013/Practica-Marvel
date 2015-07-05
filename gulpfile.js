43dbdd09e2d598027f467f1796ec878f43a9315f
Agustin Palmieri <aguja13@hotmail.com>
2015-07-05 18:51:35 -0300

    clase Gulp

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp')
+var browserify = require('browserify')
+var jadeify = require('jadeify')
+var babelify = require('babelify')
+var buffer = require('vinyl-buffer')
+var source = require('vinyl-source-stream')
+
+var stylus = require('gulp-stylus')
+var concat = require('gulp-concat-css')
+var nib = require('nib')
+
+var minify = require('gulp-minify-css')
+var uglify = require('gulp-uglify')
+
+gulp.task('build', ['styl', 'js'])
+
+gulp.task('js', function() {
+	return browserify({
+		entries: './lib/app.js', //entry point -> declaro donde comienza a compilar
+		transform: [babelify, jadeify]//transformaciones
+		})
+		.bundle()
+		.pipe(source('app.js'))//archivo destino
+		.pipe(buffer())
+		.pipe(uglify())
+		.pipe(gulp.dest('./public/'))//en donde va a estar el archivo destino
+
+})
+
+gulp.task('styl', function() {
+	return gulp.src('./lib/app.styl')//entry point de styl
+		.pipe(stylus({ use: nib()}))//inicializo stylus con nib como plugin
+		.pipe(concat('app.css'))
+		.pipe(minify())
+		.pipe(gulp.dest('./public/css'))
+})
